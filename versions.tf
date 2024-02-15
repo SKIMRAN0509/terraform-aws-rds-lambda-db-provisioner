1b05aa452ac16432b658de03ebd5244be14f413d
SKIMRAN0509 <imran.shaik@clairvoyantsoft.com>
2024-02-15 17:54:02 +0530

    udpated provider version hasicorp/aws

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.6.0, < 1.7.0"
 
   required_providers {
-    aws     = "~> 2.31"
+    aws     = "~> 5.26.0"
     archive = "~> 1.3"
     local   = "~> 1.2"
   }
